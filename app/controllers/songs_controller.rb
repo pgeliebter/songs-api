802306c7cb0a1a69b26b39b9f42223fa37ce7110
Pinny Geliebter <pgibralter@gmail.com>
2021-06-21 13:52:08 -0400

    adds destroy and updates routes for Song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,4 +25,22 @@ class SongsController < ApplicationController
     song.save
     render json: { new_song: song }
   end
+
+  def update
+    id = params[:id]
+    song = Song.find(id)
+
+    song.title = params[:title] || song.title
+    song.album = params[:album] || song.album
+    song.year = params[:year] || song.year
+    song.artist = params[:artist] || song.artist
+    song.save
+    render json: { new_song: song.class }
+  end
+
+  def destroy
+    song = Song.find(params[:id])
+    song.destroy
+    render json: { song: song }
+  end
 end
