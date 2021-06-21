fc06e686dd24ac69ed5276c8c371bd7b50bac3fd
Pinny Geliebter <pgibralter@gmail.com>
2021-06-21 13:27:04 -0400

    adds create route for Song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,4 +9,20 @@ class SongsController < ApplicationController
     songs = Song.find(id)
     render json: songs.as_json
   end
+
+  def create
+    song = Song.new(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year],
+    )
+    if song.title.downcase == "moo"
+      song.artist = "Cow"
+      song.album = "The Farm"
+      song.year = "1992"
+    end
+    song.save
+    render json: { new_song: song }
+  end
 end
