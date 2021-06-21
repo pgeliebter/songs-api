4b99fd3c202bc804c06565117b9ae102a6b67603
Pinny Geliebter <pgibralter@gmail.com>
2021-06-21 13:08:55 -0400

    adds index and show requests

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,12 @@
 class SongsController < ApplicationController
+  def index
+    songs = Song.all
+    render json: songs.as_json
+  end
+
+  def show
+    id = params[:id]
+    songs = Song.find(id)
+    render json: songs.as_json
+  end
 end
